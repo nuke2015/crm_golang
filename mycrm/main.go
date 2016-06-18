259e63d36572c3eecdea01bf30d8fb6720c099d3
锋子 <admin@bestphp.net>
2016-06-18 21:46:56 +0800

    引入首页默认路由.

--- mycrm/main.go
+++ mycrm/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	_ "mycrm/routers"
+	"mycrm/controllers"
 	"github.com/astaxie/beego"
 	"gopkg.in/mgo.v2/bson"
 )
 
 func main() {
+	
+	// index
+	beego.Router("/", &controllers.IndexController{},"get:Index")
+	
+	// control
+	beego.AutoRouter(&controllers.IndexController{})
+	beego.AutoRouter(&controllers.MainController{})
+	beego.AutoRouter(&controllers.CategoryController{})
+	beego.AutoRouter(&controllers.MenuController{})
+	beego.AutoRouter(&controllers.UserController{})
 	beego.AddFuncMap("hex",hex)
 	beego.Run()
 }
